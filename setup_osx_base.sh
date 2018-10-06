39ae3fdac97d51a395eae3b742f08a732b95fdb1
Ryan Lee <ryantlee@fb.com>
2018-10-05 17:13:14 -0700

    Add zsh syntax highlighting

--- setup_osx_base.sh
+++ setup_osx_base.sh
@@ -54,6 +54,9 @@ sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install
 # source zsh config
 source .zshrc
 
+# zsh syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 # *********************************************
 # VIM setup
 # *********************************************
